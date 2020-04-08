016c851085418ac91e1fba486d8c496a37a778a0
Jens Vanhulst <vanhulstjens@gmail.com>
2020-04-08 19:57:57 +0200

    zipped demos

--- Demos/mqtt-proto-mongodb/server/src/models/log.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const productSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  type: String,
-  value: mongoose.Schema.Types.Number
-});
-
-module.exports = mongoose.model("Log", productSchema);
