ea2ba505e85fc4443bca7e2a9aa4f40e0f870ae7
Jens Vanhulst <vanhulstjens@gmail.com>
2020-04-27 16:19:32 +0200

    update broker

--- App/broker/src/index.js
+++ App/broker/src/index.js
@@ -1,5 +1,5 @@
 const server = require("./broker.js");
-const BROKER_PORT = process.env.BROKER_PORT || 1883;
+const BROKER_PORT = 3001;
 
 server.listen(BROKER_PORT, () => {
   console.log(`Server is listening on port ${BROKER_PORT}`);
